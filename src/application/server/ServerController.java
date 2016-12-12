0ed510489be738531db66c771e9037fddd5e8608
Schorzz <Uh2stos!>
2016-12-12 23:53:45 +0100

    server view

--- src/application/server/ServerController.java
+++ src/application/server/ServerController.java
@@ -1,5 +1,16 @@
 package application.server;
 
-public class ServerController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ServerController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
