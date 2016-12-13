4b7bce1353fc1872e896cf072b08449a9b947195
Schorzz <Uh2stos!>
2016-12-13 19:22:14 +0100

    dd

--- src/csa/server/protokoll/HoelingMail.java
+++ src/csa/server/protokoll/HoelingMail.java
@@ -1,10 +1,15 @@
 package csa.server.protokoll;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class HoelingMail implements Runnable{
 
 	private Socket client;
+	private Scanner in = null;
+	private PrintWriter out = null;
 	
 	public HoelingMail(Socket socket) {
 
@@ -22,7 +27,36 @@ public class HoelingMail implements Runnable{
 		
 		//kommt noch mehr?
 		 //beenden
+		try {
+			
+			
+			
+			in=new Scanner(client.getInputStream());
+			out = new PrintWriter(client.getOutputStream(), true);
+			
+			if (authenticate()) {
+				// wenn erfolgreich, kann festgestellt werden was getan werden
+				// soll
+
+				communicate();
+
+			}
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+
+	private boolean authenticate() {
+		
+		out.println("AUTH");
+		
+		
+		return false;
 	}
 	
 }
