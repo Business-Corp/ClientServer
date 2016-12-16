b9ccea9bb5e541e69ba8643751694b6e4734dd0e
Schorzz <Uh2stos!>
2016-12-16 22:29:45 +0100

    zeitstempel zum messen der zeit hinzugefügt

--- src/csa/client/mailclient/SimpleMailTransfer.java
+++ src/csa/client/mailclient/SimpleMailTransfer.java
@@ -31,6 +31,8 @@ public class SimpleMailTransfer implements Runnable {
 	public void run() {
 		
 		try {
+			long startMillis = System.currentTimeMillis();
+			
 			anServer = new Socket("localhost", 3377);
 			in = new Scanner(anServer.getInputStream());
 			out = new PrintWriter(anServer.getOutputStream(), true);
@@ -69,6 +71,12 @@ public class SimpleMailTransfer implements Runnable {
 					}
 				}
 				in.nextLine();
+				
+				long endMillis=System.currentTimeMillis();
+				
+				Long dauer= endMillis-startMillis;
+				
+				System.out.println("client verbindung und abholen von daten in millis: "+dauer);
 				
 				anServer.close();
 				
